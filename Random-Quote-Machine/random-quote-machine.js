6cd257ca69b3fe111ef68a873cf3d25ce6699807
Catherine Etter <cetter.catherine.web@gmail.com>
2017-10-03 18:32:10 -0500

    Mobile support and quote edits

--- Random-Quote-Machine/random-quote-machine.js
+++ Random-Quote-Machine/random-quote-machine.js
@@ -1,7 +1,9 @@
 function changeQuote() {
-    $.getJSON("quotes.JSON", function(json) {
-        $("#quote").text(json[0].quote);
-        $("#author").text(json[0].author);
+    $.getJSON("https://raw.githubusercontent.com/CatherineEtter/Web-Development-Projects/master/Random-Quote-Machine/quotes.JSON", function(json) {
+        var count = Object.keys(json).length;
+        var rand = Math.floor(Math.random() * (count + 1))
+        $("#quote").text(json[rand].quote);
+        $("#author").text(json[rand].author);
    });
 }
 $(document).ready(function() {
