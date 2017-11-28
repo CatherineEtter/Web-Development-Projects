28007f96322a8e607fada99108a1c2bade347284
Catherine Etter <cetter.catherine.web@gmail.com>
2017-11-28 15:42:24 -0600

    Moved files and refined program

--- Timer/src/script.js
+++ Timer/src/script.js
@@ -1,31 +1,36 @@
 var interval;
 
+//Gets current time and counts down until 0
 function start() {
     var time = $("#disp-time").text();
     var minutes = time.substr(0,time.indexOf(':'));
     var seconds = time.substr(time.indexOf(':') + 1,time.length);
     interval = setInterval(function() {
-        if(seconds == 0 && minutes > 0) {
+        if(minutes == 0 && seconds == 1) { //Time's up
+            $("body").css("background-color","red");
+            seconds--;
+            clearInterval(interval);
+        } else if(minutes > 0 && seconds == 0) {
             minutes--;
             seconds = 59;
-        } else if(minutes >= 0 && seconds > 0){
+        } else if(seconds > 0){
             seconds--;
-        } else {
-            $("body").css("background-color","red");
-            clearInterval(interval);
         }
-        $("#disp-time").text(minutes + ":" + seconds);
+        $("#disp-time").text(minutes + ":" + (seconds < 10 ? '0' + seconds : seconds));
     },1000);
 }
+//Stops countdown and resets time
 function refresh() {
     clearInterval(interval);
     $("body").css("background-color","#003366");
     $("#disp-time").text("10:00");
 }
+//Stops countdown
 function stop() {
     clearInterval(interval);
 }
 $(document).ready(function() {
+    refresh();
     $("#btn-start").on("click",function() {
         start();
     });
